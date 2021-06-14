e736d618d07e213e305e33fb34bddadf77292fb1
Jhojan <velezjhojan9@gmail.com>
2021-06-14 18:04:21 -0500

    En productos.php he cree el grid-container y en su interior ingrese el titulo, la tabla y el filtro de busquedas

--- vista/html/productos.php
+++ vista/html/productos.php
@@ -12,5 +12,62 @@
     <?php
     require("nav.php")
     ?>
+    <main class="gestionar-productos grid-container">
+        <section class="gestionar-productos__container-title">
+            <h1 class="gestionar-productos__titulo">Gestiona Tus Productos</h1>
+        </section>
+
+
+        <section class="gestionar-productos__container-table">
+            <table class="gestionar-productos__table tabla">
+                <thead class="table-thead">
+                    <tr class="table-tr">
+                        <td class="table-td">ProCodBarr</td>
+                        <td class="table-td">Descripcion</td>
+                        <td class="table-td">UbicacionFisica</td>
+                        <td class="table-td">Presentacion</td>
+                        <td class="table-td">UnidadMedida</td>
+                        <td class="table-td">PrecioVenta</td>
+                        <td class="table-td">FechaVenc</td>
+                        <td class="table-td">Laboratorio</td>
+                        <td class="table-td">CodPro</td>
+                        <td class="table-td">CodInvt</td>
+                    </tr>
+                </thead>
+                <tbody></tbody>
+            </table>
+        </section>
+
+
+        <section class="gestionar-productos__container-filter">
+            <div class="gestionar-productos_filtro">
+                <h2 class="gestionar-productos_filtro-titulo">Busca un Producto</h2>
+                <form action="">
+                    <label for="product-id">Por codigo de barras del producto</label>
+                    <br>
+                    <input type="text" class="gestionar-productos_filtro-id" id="product-id">
+                    <br>
+                    <label for="product-nombre">Por nombre del producto</label>
+                    <br>
+                    <input type="text" class="gestionar-productos_filtro-nombre" id="product-nombre">
+                    <br>
+                    <label for="product-proveedor">Por nombre del proveedor</label>
+                    <br>
+                    <input type="text" class="gestionar-productos_filtro-proveedor" id="product-proveedor">
+                    <br>
+                    <label for="product-id">Por codigo de barras del producto</label>
+                    <br>
+                    <select name="presentacion" class="gestionar-productos_filtro-presentacion" id="product-presentacion">
+                        <option value="null">Presentacion</option>
+                        <option value="Pre">Pre1</option>
+                        <option value="Pre">Pre2</option>
+                        <option value="Pre">Pre3</option>
+                        <option value="Pre">Pre4</option>
+                    </select>
+                </form>
+                <div></div>
+            </div>
+        </section>
+    </main>
 </body>
 </html>
