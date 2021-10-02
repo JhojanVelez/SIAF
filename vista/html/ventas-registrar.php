2013866384455f3ae9e86e754f9ff8b64725cf85
Jhojan <velezjhojan9@gmail.com>
2021-10-02 14:39:59 -0500

    En el modulo de ventas, cree los dialogs para poder confirmar/registrar la venta(HTML, CSS y JS), como tambien cree los dialogs por si es necesario registrar un cliente(HTML, CSS)

--- vista/html/ventas-registrar.php
+++ vista/html/ventas-registrar.php
@@ -469,16 +469,125 @@
                     Recuerda que una vez cancelada, se borraran todos los productos a vender y toda la infomacion relacionada a esta venta.
                 </p>
 
-                </div>
                 <div class="registrar-ventas__modal-cancelar-venta-btns-container dialog-container-bts">
                     <button class="registrar-ventas__modal-cancelar-venta-btn-cancelar dialog-btn boton">Cancelar</button>
                     <button class="registrar-ventas__modal-cancelar-venta-btn-confirmar dialog-btn boton">Confirmar</button>
                 </div>
+            </dialog>
+
+            <!-- Estos son los modals para poder confirmar una venta -->
+
+            <dialog class="registrar-ventas__modal-confirmar-venta">
+                <h2 class="registrar-ventas__modal-confirmar-venta-title dialog-title">Informaci&oacute;n adicional</h2>
+                <form class="registrar-ventas__modal-confirmar-venta-form dialog-main-content">
+                    <div class="registrar-ventas__modal-confirmar-venta-form-info-venta">
+                        <h3 class="registrar-ventas__modal-confirmar-venta-item-title">Numero de documento del cliente</h3>
+                        <input class="registrar-ventas__modal-confirmar-venta-item" type="text" placeholder="Documento">
+                        <h3 class="registrar-ventas__modal-confirmar-venta-item-title">Forma de pago</h3>
+                        <select class="registrar-ventas__modal-confirmar-venta-item">
+                            <option value="">Forma de pago</option>
+                        </select>
+                    </div>
+                    <div class="registrar-ventas__modal-confirmar-venta-form-info-precios">
+                        <h3 class="registrar-ventas__modal-confirmar-venta-item-title">Total a Pagar</h3>
+                        <p class="registrar-ventas__modal-confirmar-venta-item registrar-ventas__modal-confirmar-venta-item--total">$ 56000 Pesos</p>
+                        <h3 class="registrar-ventas__modal-confirmar-venta-item-title">Recibe</h3>
+                        <input class="registrar-ventas__modal-confirmar-venta-item" type="text" >
+                        <h3 class="registrar-ventas__modal-confirmar-venta-item-title">Cambio</h3>
+                        <p class="registrar-ventas__modal-confirmar-venta-item">$0 Pesos</p>
+                    </div>
+                </form>
+                <div class="registrar-ventas__modal-confirmar-venta-btns-container dialog-container-bts">
+                    <button class="registrar-ventas__modal-confirmar-venta-btn-cancelar dialog-btn boton">Cancelar</button>
+                    <button class="registrar-ventas__modal-confirmar-venta-btn-confirmar dialog-btn boton">Confirmar</button>
+                </div>
+            </dialog>
+
+            <dialog class="registrar-ventas__modal-venta-exitosa dialog-process-result">
+                <h2>¡Exelente!</h2>
+                <p>Has registrado tu venta exitosamente</p>
+                <button class="registrar-ventas__modal-venta-exitosa-btn dialog-process-result__btn boton" >Ok</button>
+            </dialog>
+            
+            <dialog class="registrar-ventas__modal-venta-fallo-por-cliente dialog-process-result">
+                <h2>¡Cliente no registrado!</h2>
+                <p>Desea registrarlo o pasarlo como anonimo</p>
+                <button class="registrar-ventas__modal-venta-fallo-por-cliente-btn-registrar dialog-process-result__btn boton">Anonimo</button>
+                <button class="registrar-ventas__modal-venta-fallo-por-cliente-btn-anonimo dialog-process-result__btn boton">Registrar</button>
+            </dialog>
+
+
+            <!-- Estos son los modals para cuando no se encuentre el cliente y se necesite agregar uno -->
+            <dialog class="registrar-ventas__modal-agregar-cliente">
+                <h2 class="registrar-ventas__modal-agregar-cliente-title dialog-title">Registra Nuevos Clientes</h2>
+                <form class="registrar-ventas__modal-agregar-cliente-form dialog-main-content">
+                    <input type="text" placeholder="Numero de documento del cliente">
+                    <input type="text" placeholder="Correo">
+                    <input type="text" placeholder="Nombres">
+                    <input type="text" placeholder="Direcci&oacute;n">
+                    <input type="text" placeholder="Apellidos">
+                    <input type="text" placeholder="Telefono">
+                </form>
+                <div class="registrar-ventas__modal-agregar-cliente-btns-container dialog-container-bts">
+                    <button class="registrar-ventas__modal-agregar-cliente-btn-cancelar boton dialog-btn">Cancelar</button>
+                    <button class="registrar-ventas__modal-agregar-cliente-btn-añadir boton dialog-btn">A&ntilde;adir</button>
+                </div>
+            </dialog>
+
+            <dialog  class="registrar-ventas__modal-agregar-cliente-confirmacion">
+                <h2 class="registrar-ventas__modal-agregar-cliente-title-confirmacion dialog-title">Verifica los Datos</h2>
+                <p class="registrar-ventas__modal-agregar-cliente-text-confirmacion dialog-text">
+                    ¿Estas seguro de registrar este cliente?<br>
+                    Recuerda revisar detenidamente la informacion del cliente que estas registrando.
+                </p>
+                <div class="registrar-ventas__modal-agregar-cliente-info-confirmacion dialog-main-content">
+                    <section class="registrar-ventas__modal-agregar-cliente-info-item-confirmacion">
+                        <h3>DOCUMENTO DEL CLIENTE</h3>
+                        <p>________________________________________________</p>
+                    </section>
+                    <section class="registrar-ventas__modal-agregar-cliente-info-item-confirmacion">
+                        <h3>CORREO</h3>
+                        <p>________________________________________________</p>
+                    </section>
+                    <section class="registrar-ventas__modal-agregar-cliente-info-item-confirmacion">
+                        <h3>NOMBRE/S DEL CLIENTE</h3>
+                        <p>________________________________________________</p>
+                    </section>
+                    <section class="registrar-ventas__modal-agregar-cliente-info-item-confirmacion">
+                        <h3>DIRECCI&Oacute;N</h3>
+                        <p>________________________________________________</p>
+                    </section>
+                    <section class="registrar-ventas__modal-agregar-cliente-info-item-confirmacion">
+                        <h3>APELLIDO/S DEL CLIENTE</h3>
+                        <p>________________________________________________</p>
+                    </section>
+                    <section class="registrar-ventas__modal-agregar-cliente-info-item-confirmacion">
+                        <h3>TELEFONO</h3>
+                        <p>________________________________________________</p>
+                    </section>
+                </div>
+                <div class="registrar-ventas__modal-agregar-cliente-confirmacion-btns-container dialog-container-bts">
+                    <button class="registrar-ventas__modal-agregar-cliente-confirmacion-btn-cancelar dialog-btn boton">Volver Atras</button>
+                    <button class="registrar-ventas__modal-agregar-cliente-confirmacion-btn-confirmar dialog-btn boton">Confirmar</button>
+                </div>
+            </dialog>
+
+            <dialog class="registrar-ventas__modal-agregacion-exitosa dialog-process-result">
+                <h2>¡Exelente!</h2>
+                <p>Has registrado un nuevo cliente exitosamente</p>
+                <button class="registrar-ventas__modal-agregacion-exitosa-btn dialog-process-result__btn boton" >Ok</button>
+            </dialog>
+            
+            <dialog class="registrar-ventas__modal-agregacion-fallo dialog-process-result">
+                <h2>¡Algo salio mal!</h2>
+                <p>Este cliente no pudo ser registrado, porque posiblemente ya esta registrado en el sistema</p>
+                <button class="registrar-ventas__modal-agregacion-fallo-btn dialog-process-result__btn boton" >Ok</button>
             </dialog>
         </section>
     </main>
 
     <script src="../js/modulo_ventas_registrar_ver_modal_productos.js"></script>
     <script src="../js/modulo_ventas_registrar_cancelar_venta.js"></script>
+    <script src="../js/modulo_ventas_confirmar_venta.js"></script>
 </body>
 </html>
