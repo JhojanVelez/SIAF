ac95174361fd6db1d694af3a89c02178eef74a10
Jhojan <velezjhojan9@gmail.com>
2021-10-10 11:14:12 -0500

    feat: Agregacion del controlador de ventasMenuCards y modificacion de los links del archivo ventas-menu-cards para que apunten desde el index hasta la carpeta public

--- vista/ventas-menu-cards.php
+++ vista/ventas-menu-cards.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ventas</title>
-    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="public/css/main.css">
     <style>
         .nav__li:nth-child(4) {
             border-bottom: 2px solid var(--color-button-text);
@@ -25,19 +25,19 @@
                 <section class="card box-shadow">
                     <h2 class="card__title">REGISTRAR VENTAS</h2>
                     <div class="card__img">
-                        <img src="../imagenes/ventas-registrar-ventas.svg" alt="">
+                        <img src="public/imagenes/ventas-registrar-ventas.svg" alt="">
                     </div>
                     <button class="card__button boton">
-                        <a href="ventas-registrar.php">Ingresar</a>
+                        <a href="ventasRegistrar">Ingresar</a>
                     </button>
                 </section>
                 <section class="card box-shadow">
                     <h2 class="card__title">CONSULTAR VENTAS</h2>
                     <div class="card__img">
-                        <img src="../imagenes/ventas-consultar-ventas.svg" />
+                        <img src="public/imagenes/ventas-consultar-ventas.svg" />
                     </div>
                     <button class="card__button boton">
-                        <a href="ventas-consultar-ventas.php">Ingresar</a>
+                        <a href="ventasConsultarVentas">Ingresar</a>
                     </button>
                 </section>
         </article>
