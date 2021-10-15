0ede0a726f689c0e9bf4c84e1d0eab56f1bc94c8
Jhojan <velezjhojan9@gmail.com>
2021-10-15 18:49:04 -0500

    fix: en el archivo modulo_productos_seleccion_de_producto.js modifique el selector, para que seleccionara solo los td que estan en el tbody

--- public/js/modulo_productos_seleccion_de_producto.js
+++ public/js/modulo_productos_seleccion_de_producto.js
@@ -5,7 +5,7 @@
 let product = btnInhabilitar; /* Le asigno el btn, solo por usar un elemento HTML, es decir para que no nos de error en la linea 9*/
     d.addEventListener("click", e => {
 
-        if(e.target.matches(".productos__table td")) {
+        if(e.target.matches(".productos__table tbody td")) {
             product.classList.remove("product-selected")
             e.target.parentElement.classList.toggle("product-selected")
             let idProducto = e.target.parentElement.firstElementChild.textContent
