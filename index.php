456b336e6d62c49a18f40b9f51d8ce627d30c8ba
Jhojan <velezjhojan9@gmail.com>
2021-10-28 11:27:12 -0500

    Feat: agregue comentarios al index y al router

--- index.php
+++ index.php
@@ -7,6 +7,12 @@ require("libs/conexion_BD.php");
 require("libs/controlador.php");
 require("libs/router.php");
 
+/*
+A la hora de instanciar nuestro router, se ejecuta el constructor, este validara la url,
+requiere el controlador, el controlador llama al modelo y si hay metodo, lo invoca, si no
+ejecuta el metodo por defecto, que es traer todos los datos
+*/
 $router = new Router;
+
 
 ?>
