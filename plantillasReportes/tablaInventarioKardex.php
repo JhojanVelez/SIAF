b03f0d74d2609600e793a9f56a4c4e679277cb82
Jhojan <velezjhojan9@gmail.com>
2021-11-05 12:22:00 -0500

    Feat: Cree toda la funcionalidad de poder generar un reporte, con los estilos del color segun el stock, asi que tambien agregue estilos a la plantilla

---
+++ plantillasReportes/tablaInventarioKardex.php
@@ -0,0 +1,58 @@
+<div class="platilla-reporte-main__content">
+    <p class="platilla-reporte-main__title">Reporte de Kardex</p>
+    <p class="platilla-reporte-main__h3">Informacion adicional</p>
+    <p>Cantidad Total de Registros: <?php echo(count($this->data)) ?> </p>
+</div>
+<div class="platilla-reporte__container-table">
+    <table class="platilla-reporte__table platilla-reporte__table--kardex table">
+        <thead class="table-thead table-thead--kardex">
+            <tr class="table-tr">
+                <td>codInvt</td>
+                <td>ProCodBarras</td>
+                <td>Producto</td>
+                <td>Total Entradas</td>
+                <td>Total Salidas</td>
+                <td>Stock</td>
+                <td>Ubicacion Fisica</td>
+                <td>Costo Producto</td>
+                <td>Precio Venta</td>
+                <td>Producto Presentacion</td>
+                <td>Laboratorio</td>
+                <td>NitProveedor</td>
+                <td>Proveedor</td>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($this->data as $key => $value): ?>
+                <tr class="kardex__table-tboby-tr">
+                    <td><?php echo $value["InvCodigo"] ?></td>
+                    <td><?php echo $value["ProCodBarras"] ?></td>
+                    <td><?php echo $value["ProDescripcion"] ?></td>
+                    <td><?php echo $value["InvTotalEntradas"] ?></td>
+                    <td><?php echo $value["InvTotalSalidas"] ?></td>
+                    <td 
+                    class="
+                        <?php
+                            if($value["InvStock"] <= 0) {
+                                echo("kardex__table-stock-red");
+                            } else if ($value["InvStock"] > 0 && $value["InvStock"] <= 10) {
+                                echo("kardex__table-stock-orange");
+                            } else {
+                                echo("kardex__table-stock-green");
+                            }
+                        ?>
+                    ">
+                    <?php echo $value["InvStock"] ?>
+                    </td>
+                    <td><?php echo $value["ProUbicacionFisica"] ?></td>
+                    <td><?php echo $value["CostoProducto"] ?></td>
+                    <td><?php echo $value["ProPrecioVenta"] ?></td>
+                    <td><?php echo $value["ProPresentacion"] ?></td>
+                    <td><?php echo $value["ProLaboratorio"] ?></td>
+                    <td><?php echo $value["ProNIT"] ?></td>
+                    <td><?php echo $value["ProNombre"] ?></td>
+                </tr>
+            <?php endforeach;?>
+        </tbody>
+    </table>
+</div>
